67cfd60abf646ffee03f8fd802ed59037d05700d
Miguel Angel <miguel_qdoq@hotmail.com>
2024-10-16 17:13:02 -0600

    tercer actualizacion

---
+++ tipos-de-dato.js
@@ -0,0 +1,50 @@
+console.log(1);
+console.log("hola mundo");
+console.log(typeof 42);
+console.log(typeof "Veinticinco");
+console.log(typeof -666);
+console.log(typeof true);
+console.log(typeof 0);
+console.log(typeof "");
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof FALSE);
+console.log(typeof "###");
+console.log(typeof "# 1324");
+console.log(typeof 8190);
+console.log(typeof 5 + 8);
+console.log(typeof number + 5);
+console.log(typeof "2" + " " + 10);
+console.log(typeof "123456789");
+console.log(typeof ".456");
+console.log(typeof "/");
+console.log(typeof "0");
+console.log(typeof 0);
+console.log(typeof 0.123);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
